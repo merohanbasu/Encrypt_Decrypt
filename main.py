a09c89c75a6e050fe00619b40ab0d98216775219
Rohan Jyoti Basu <merohanbasu@gmail.com>
2023-09-17 20:55:50 +0530

    Modified

--- main.py
+++ main.py
@@ -1,48 +1,73 @@
-import random
-import string
+from tkinter import messagebox, simpledialog, Tk
 
-q = input("You want to code or decode? ")
 
-match q.lower():
-    case "code":
-        s = input("Enter a word to code: ")
-        words = s.split(" ")
-        n = len(words)
+def is_even(number):
+    # even check
+    return number % 2 == 0
 
-        # coding
 
-        randm_char1 = ''.join(random.choice(string.ascii_letters) for _ in range(3))
-        randm_char2 = ''.join(random.choice(string.ascii_letters) for _ in range(3))
+def get_even_letters(message):
+    # getting even pos characters
+    even_letters = []
+    for counter in range(0, len(message)):
+        if is_even(counter):
+            even_letters.append(message[counter])
+    return even_letters
 
-        if (n < 3):
-            new_char = []
-            for word in words:
-                new_char.append(word[::-1])
-            ans = "".join(new_char)
-        else:
-            new_char = []
-            s = s[1:] + s[0]
-            s = s.split()
-            for word in s:
-                new_char.append(word)
-            ans = "".join(new_char)
-            ans = randm_char1 + ans + randm_char2
 
-        print(ans)
-    case "decode":
-        s = input("Enter a word to Decode: ")
-        n = len(s)
+def get_odd_letters(message):
+    # getting odd pos characters
+    odd_letters = []
+    for counter in range(0, len(message)):
+        if not is_even(counter):
+            odd_letters.append(message[counter])
+    return odd_letters
 
-        # decoding
 
-        if(n < 3):
-            new_char = s[::-1]
-        else:
-            s = s[3:-3]
-            new_char = s[len(s) - 1] + s[:-1]
-        print(new_char)
-    case _:
-        print("Invalid Input")
+def swap_letters(message):
+    letter_list = []
+    if not is_even(len(message)):
+        message = message + 'x'
+    even_letters = get_even_letters(message)
+    odd_letters = get_odd_letters(message)
+    for counter in range(0, int(len(message) / 2)):
+        letter_list.append(odd_letters[counter])
+        letter_list.append(even_letters[counter])
+    new_message = ''.join(letter_list)
+    return new_message
+
+def get_task():
+    choice = simpledialog.askstring('Choice', 'Do you want to encrypt or decrypt?')
+    return choice
+
+
+def get_message():
+    text = simpledialog.askstring('Message', 'Enter the text')
+    return text
+
+
+root = Tk()
+root.withdraw()
+# infinite loop for the user
+while True:
+    # choice for encyption or decryption
+    task = get_task()
+
+    if task == 'encrypt':
+        # message from the user
+        message = get_message()
+        encrypted = swap_letters(message)
+        # to show the encrypted message
+        messagebox.showinfo('Ciphertext of the secret message is:', encrypted)
+
+    elif task == 'decrypt':
+        # message from the user
+        message = get_message()
+        decrypted = swap_letters(message)
+        # to showthe encrypted message
+        messagebox.showinfo('Plaintext of the secret message is:', decrypted)
+    else:
+        break
 
 
 
